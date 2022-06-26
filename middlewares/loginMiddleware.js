5eb83edcebd03a14e7aad7ed8f31293663dc6c70
isagomachado <isa_gomes_machado@hotmail.com>
2022-06-26 18:13:22 -0300

    Requisito 4 - feito - lint

--- middlewares/loginMiddleware.js
+++ middlewares/loginMiddleware.js
@@ -1,26 +1,18 @@
 const loginMiddleware = (req, res, next) => {
   const { email, password } = req.body;
+  const emailValidation = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
 
-  const emailValidation =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-
-  if(!email) {
-    return res.status(400)
-      .json({ message: 'O campo \"email\" é obrigatório' })
+  if (!email) {
+    return res.status(400).json({ message: 'O campo email é obrigatório' });
   }
-
-  if(!(emailValidation.test(email))) {
-    return res.status(400)
-      .json({ message: 'O \"email\" deve ter o formato \"email@email.com\"' })
+  if (!(emailValidation.test(email))) {
+    return res.status(400).json({ message: 'O email deve ter o formato email@email.com' });
   }
-
-  if(!password) {
-    return res.status(400)
-      .json({ message: 'O campo \"password\" é obrigatório' })
+  if (!password) {
+    return res.status(400).json({ message: 'O campo password é obrigatório' });
   }
-
-  if(!(password.length >= 6)) {
-    return res.status(400)
-      .json({ message: 'O \"password\" deve ter pelo menos 6 caracteres' })
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O password deve ter pelo menos 6 caracteres' });
   }
 
   next();
